454b0004ab1ebd3c81467c1201a8dee43c985249
Mehrdad Mehralian <dadwic0@gmail.com>
2023-05-08 23:55:45 +0300

    fix: change the path of tests folder

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from '..';
+import Home from '../pages';
 
 describe('Home', () => {
   it('renders a heading', () => {
--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Home from '..';
+import Home from '../pages';
 
 it('renders homepage unchanged', () => {
   const { container } = render(<Home />);
