fd3c20c77d82c8b90e423a793205d808e0316525
Mehrdad Mehralian <dadwic0@gmail.com>
2023-06-12 22:32:56 +0300

    update gtm container id and script

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AOS from 'aos';
@@ -90,15 +91,19 @@ export default function MyApp(props) {
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtag.GTM_CONTAINER_ID}');`,
+          }}
+        />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
         <Component {...pageProps} />
         <Analytics />
       </ThemeProvider>
